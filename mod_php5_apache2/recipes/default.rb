9272af92e09ee5763f75dba82615740ad1cf3b6a
Stephanie King <stepking@amazon.com>
2014-04-29 14:30:53 -0700

    removed httpd and httpd-tools which cause php55-devel install to fail

--- mod_php5_apache2/recipes/default.rb
+++ mod_php5_apache2/recipes/default.rb
@@ -1,5 +1,13 @@
 include_recipe 'apache2'
 
+bad_packages =  ["httpd","httpd-tools"]
+
+bad_packages.each do |pkg|
+  package pkg do
+    action :remove
+  end
+end
+
 node[:mod_php5_apache2][:packages].each do |pkg|
   package pkg do
     action :install
