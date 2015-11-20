333c6263988cff04e5b34ad149df617273e31210
jamesoflol <james.lol@hale.com.au>
2015-11-20 12:25:28 +1100

    .

--- nfs_mount/recipes/default.rb
+++ nfs_mount/recipes/default.rb
@@ -1,17 +1,3 @@
-directory "/testj" do
-  owner 'root'
-  group 'root'
-  mode '0777'
-  action :create
-end
-
-file "/testj/1" do
-  content node["deploy"]["moodle"]["memcached"]["host"]
-  mode '0777'
-  owner 'root'
-  group 'root'
-end
-
 directory "/mnt/nfs" do
   owner 'root'
   group 'root'
@@ -27,8 +13,7 @@ directory "/mnt/nfs/moodledata" do
 end
 
 mount "/mnt/nfs/moodledata" do
-  #device node[:opsworks][:layers][:memcached][:instances][0] + ":/vol/moodledata"
-  device node["deploy"]["appshortname"]["memcached"]["host"] + ":/vol/moodledata"
+  device node["deploy"]["moodle"]["memcached"]["host"] + ":/vol/moodledata"
   device "memcached1:/vol/moodledata"
   fstype "nfs"
   options "rw"
